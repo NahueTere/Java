7336430bb8e37a4a4ea7f0d1e7043e290baa2f2e
NahueTere <nahuelterenzanito@gmail.com>
2026-04-06 15:17:40 -0300

    1.3

--- Ejercicio1_3.java
+++ Ejercicio1_3.java
@@ -1,28 +1,32 @@
 import javax.swing.*;
 
-public class Ejercicio1_2 extends JFrame {
+public class Ejercicio1_3 extends JFrame {
 
-    public Ejercicio1_2() {
-        setTitle("Ejercicio 1.2");
+    public Ejercicio1_3() {
+        setTitle("Ejercicio 1.3");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JTextField inputField = new JTextField(10);
+        JTextField añoactualField = new JTextField(10);
         JButton submitButton = new JButton("Enviar");
-        JLabel resultLabel = new JLabel("El año que viene tendras: ");
+        JLabel resultLabel = new JLabel("tienes: ");
 
         submitButton.addActionListener(e -> {
             try {
-                int num = Integer.parseInt(inputField.getText());
-                int edad =num+1;
-                resultLabel.setText("El año que viene tendras: " + edad);
+                int añonacimiento = Integer.parseInt(inputField.getText());
+                int añoactual = Integer.parseInt(añoactualField.getText());
+                int edad = añoactual-añonacimiento;
+                resultLabel.setText("tienes: " + edad + " años");
             } catch (NumberFormatException ex) {
                 resultLabel.setText("Entrada inválida. Introduce un número válido.");
             }
         });
 
         JPanel panel = new JPanel();
-        panel.add(new JLabel("Escriba su edad: "));
+        panel.add(new JLabel("Escriba su año de nacimiento: "));
         panel.add(inputField);
+        panel.add(new JLabel("Escriba el año actual: "));
+        panel.add(añoactualField);
         panel.add(submitButton);
         panel.add(resultLabel);
 
@@ -33,7 +37,7 @@ public class Ejercicio1_2 extends JFrame {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            Ejercicio1_2 ejercicio = new Ejercicio1_2();
+            Ejercicio1_3 ejercicio = new Ejercicio1_3();
             ejercicio.setVisible(true);
         });
     }
